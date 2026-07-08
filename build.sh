fefa51c527330acc4ccaf7247378b171d00c389f
nurbaitenov <baitenovnur@gmail.com>
2026-07-08 09:17:56 +0000

    added

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#below command builds 3 Centos and 3 ubunto Instances
+# for ansible inventory from Farrukh's class
+bash -c "$(curl https://bucket-to-check-aws-tasks.s3.amazonaws.com/AWS/scripts/shared_scripts/ansible_menu.sh)" 
+
+# bash build.sh
