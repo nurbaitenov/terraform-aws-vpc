22b718f10169090787050310920e1cb98b6731e3
nurbaitenov <baitenovnur@gmail.com>
2026-07-08 09:14:55 +0000

    added

---
+++ instalation.sh
@@ -0,0 +1,14 @@
+#Terraform install
+
+
+#!/bin/bash
+git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
+echo 'export PATH=$PATH:$HOME/.tfenv/bin' >> ~/.bashrc
+source ~/.bashrc 
+
+# bash  installation.sh
+# source ~/.bashrc 
+
+# tfenv    install   1.1.1 
+# tfenv    use       1.1.1 
+# terraform version
---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
