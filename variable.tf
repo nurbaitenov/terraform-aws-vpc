0ae043e02f1318fc2ed2b87ff84c556bf9779ada
nurbaitenov <baitenovnur@gmail.com>
2026-07-08 10:14:08 +0000

    added

--- variable.tf
+++ variable.tf
@@ -1,23 +1,29 @@
 variable "cidr_block" {
     description = "Please provide CIDR block for VPC"
-    type = "string"
+    type = string
     default = ""
 }
 
 variable "region" {
     description = "Please provide a region"
-    type = "string"
+    type = string
     default = ""
 }
 
 variable "public_subnet1" {
-    description = "Please provide subnet1"
-    type = "string"
+    description = "Please provide public subnet1"
+    type = string
     default = ""
 }
 
 variable "public_subnet2" {
-    description = "Please provide subnet1"
-    type = "string"
+    description = "Please provide public subnet2"
+    type = string
+    default = ""
+}
+
+variable "private_subnet1" {
+    description = "Please provide private subnet1"
+    type = string
     default = ""
 }
--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,34 @@
 # Create a VPC
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_blcok
+  cidr_block = var.cidr_block
   instance_tenancy = "default"
   enable_dns_hostnames = true   # needs for rds
   enable_dns_support = true     # needs for rds
 }
 
-resource "aws_subnet1" "public1" {
+resource "aws_subnet" "public1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet1
   map_public_ip_on_launch = true # each public IP addr should have associate public IP addr
 }
 
-resource "aws_subnet2" "public2" {
+resource "aws_subnet" "public2" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet1
+  cidr_block = var.public_subnet2
   map_public_ip_on_launch = true # each public IP addr should have associate public IP addr
+}
+
+
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet1
+  map_public_ip_on_launch = false
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
 }
